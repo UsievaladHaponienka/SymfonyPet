7a57a52ee9a21c520d72e3a28f5b4167169fd705
Usievalad Haponienka <usievalad.haponienka@gmail.com>
2022-10-28 13:31:11 +0500

    SymfonyStudy
     - Copy-pasted templates. Added show action

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,26 +4,33 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
+    private EntityRepository $repository;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->entityManager = $entityManager;
+        $this->repository = $entityManager->getRepository(Movie::class);
     }
 
-    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
+    #[Route('/movies/', name: 'movies', methods: ['GET', 'HEAD'])]
     public function index(): Response
     {
-        $repository = $this->entityManager->getRepository(Movie::class);
-        $movies = $repository->findAll();
-        return $this->render('movies/index.html.twig', [
-            'movies' => $movies,
-        ]);
+        $movies = $this->repository->findAll();
+        return $this->render('movies/index.html.twig', compact('movies'));
+    }
+
+    #[Route('movies/{id}', name: 'movies.show', methods: ['GET', 'HEAD'])]
+    public function show($id)
+    {
+        $movie = $this->repository->find($id);
+
+        return $this->render('movies/show.html.twig', compact('movie'));
+
     }
 }
