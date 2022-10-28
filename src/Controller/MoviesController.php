acd243f7b3f5e978aa11e8c7ed98c8474901e4af
Usievalad Haponienka <usievalad.haponienka@gmail.com>
2022-10-28 15:08:16 +0500

    SymfonyStudy
     - Added movie form type. Processed create action.

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Form\MovieFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,19 +23,58 @@ class MoviesController extends AbstractController
         $this->repository = $entityManager->getRepository(Movie::class);
     }
 
-    #[Route('/movies/', name: 'movies', methods: ['GET', 'HEAD'])]
+    #[Route('/movies/', name: 'movies.index', methods: ['GET', 'HEAD'])]
     public function index(): Response
     {
         $movies = $this->repository->findAll();
         return $this->render('movies/index.html.twig', compact('movies'));
     }
 
+    #[Route('movies/create', name: 'movies.create')]
+    public function create(Request $request): Response
+    {
+        $movie = new Movie();
+        $form = $this->createForm(MovieFormType::class, $movie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //TODO: Debug this method
+            $newMovie = $form->getData();
+
+            /** @var UploadedFile $imagePath */
+            $imagePath = $form->get('imagePath')->getData();
+            if ($imagePath) {
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $newMovie->setImagePath('/uploads/' . $newFileName);
+            }
+
+            $this->repository->save($newMovie, true);
+
+            return $this->redirectToRoute('movies.index');
+        }
+
+        return $this->render('movies/create.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
     #[Route('movies/{id}', name: 'movies.show', methods: ['GET', 'HEAD'])]
-    public function show($id)
+    public function show($id): Response
     {
         $movie = $this->repository->find($id);
 
         return $this->render('movies/show.html.twig', compact('movie'));
-
     }
+
+
 }
